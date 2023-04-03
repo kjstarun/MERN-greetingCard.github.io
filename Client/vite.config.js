39c2ad01e7d381d5124e3e7ffc33d42fe6071d87
kjstarun <kjstarun@gmail.com>
2023-04-03 15:47:44 +0530

    updated

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MERN-greetingCard.github.io/client/",
+  base: "/MERN-greetingCard.github.io/Client/",
 });
