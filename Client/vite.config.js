4b4614ffa6ec3cb861ae9a293bb68f7e9bce2c8c
kjstarun <kjstarun@gmail.com>
2023-04-04 19:18:26 +0530

    up

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,16 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/Client/",
-// });
-
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import react from "vite-preset-react";
 import svgr from "vite-plugin-svgr";
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
