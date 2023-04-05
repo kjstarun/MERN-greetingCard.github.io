ccecbc2e5d45aa24384ada1bcaf887ce7f4ff9cb
kjstarun <kjstarun@gmail.com>
2023-04-05 15:06:33 +0530

    updated

--- Client/src/components/cards/cardGenerator.jsx
+++ Client/src/components/cards/cardGenerator.jsx
@@ -1,22 +1,21 @@
 import Card from "react-bootstrap/Card";
-import HeartSVG from "../../assets/images/heart-svg";
-import ViewSVG from "../../assets/images/View-svg";
+import HeartSVG from "../../assets/svg/heart-svg";
+import ViewSVG from "../../assets/svg/View-svg";
+import birthday from "../../assets/images/birthday.webp";
+import { CARDS_DATA } from "../../utilities/card-data";
 
 const CardGenerator = () => {
   const temp = [1, 2, 3];
   return (
     <div className="cards-container">
-      {temp.map((item, index) => {
+      {CARDS_DATA.map((item, index) => {
         return (
           <div className="card-parent">
             <Card style={{ width: "18rem" }} key={index}>
-              <Card.Img
-                variant="top"
-                src="https://www.shutterstock.com/image-vector/happy-birthdaybeautiful-greeting-card-scratched-260nw-554302417.jpg"
-              />
+              <Card.Img variant="top" src={birthday} alt="Image" />
             </Card>
             <Card.Body className="card-details">
-              <span>Card Title</span>
+              <span>{item.title}</span>
               <span className="like-view-details">
                 <HeartSVG />
                 <ViewSVG />
---
+++ Client/src/utilities/card-data.js
@@ -0,0 +1,17 @@
+export const CARDS_DATA = [
+  {
+    id: 1,
+    title: "Tarun",
+    imageSource: "",
+  },
+  {
+    id: 2,
+    title: "Hareesh",
+    imageSource: "anniversary",
+  },
+  {
+    id: 3,
+    title: "Ashwary",
+    imageSource: "wedding",
+  },
+];
